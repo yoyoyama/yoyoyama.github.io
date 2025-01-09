4ccbabf44d1c9025d0f5e7f3dec9e3d189a0ca07
yoyoyama <mt.yoyoyama@gmail.com>
2025-01-09 19:24:16 +0900

    eslint導入

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['dist'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+];
