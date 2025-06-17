6ec3690e2d2f7a9f2e0910e726be07e00792aaf4
yoyoyama <mt.yoyoyama@gmail.com>
2025-06-18 00:25:59 +0900

    乱数の分布を中央に偏らせる

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './assets/css/style.css';
-import { debounce } from './utils';
+import { centeredRandom, debounce } from './utils';
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
@@ -9,8 +9,14 @@ const lineWidth = 2;
 const width = 16;
 const height = 10;
 
-function drawGrass(x: number, y: number, alpha: number) {
+function drawGrass() {
+  const x = centeredRandom() * canvas.width;
+  const y = centeredRandom() * canvas.height;
+  const alpha = centeredRandom();
+
   ctx.globalAlpha = alpha;
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
 
   ctx.beginPath();
   ctx.moveTo(x - width / 2, y - height / 1.25);
@@ -26,18 +32,13 @@ function drawGrass(x: number, y: number, alpha: number) {
 
 function drawAllGrass() {
   for (let i = 0; i < total; i++) {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    const alpha = Math.random() * (1 - 0.1) + 0.1;
-    drawGrass(x, y, alpha);
+    drawGrass();
   }
 }
 
 function init() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = color;
 
   drawAllGrass();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
+// 2つの乱数の平均を返す（分布を中央に偏らせた乱数を返す）
+export function centeredRandom() {
+  return (Math.random() + Math.random()) / 2;
+}
+
 // コールバック関数を指定した遅延時間後に実行する
 // 連続して呼び出された場合は最後の呼び出しのみが実行される
 export function debounce(callback: () => void, delay: number) {
