398fa6f1a03ee68a4ba0d264e9d072e43d4f7d0f
yoyoyama <mt.yoyoyama@gmail.com>
2025-06-18 00:26:29 +0900

    衝突判定を入れる

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,62 @@
 import './assets/css/style.css';
 import { centeredRandom, debounce } from './utils';
 
+const avatar = document.querySelector('.avatar') as HTMLDivElement;
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 const total = import.meta.env.VITE_GITHUB_TOTAL_CONTRIBUTIONS;
-const color = '#2BC45E';
-const lineWidth = 2;
-const width = 16;
-const height = 10;
+const grassColor = '#2BC45E';
+const grassLineWidth = 2;
+const grassWidth = 16;
+const grassHeight = 10;
 
-function drawGrass() {
-  const x = centeredRandom() * canvas.width;
-  const y = centeredRandom() * canvas.height;
+function getGrassPosition(obstacles: DOMRect[]): { x: number; y: number } {
+  let x: number, y: number;
+
+  do {
+    x = Math.random() * canvas.width;
+    y = Math.random() * canvas.height;
+  } while (
+    // 衝突判定
+    obstacles.some(
+      (obstacle) =>
+        x + grassWidth >= obstacle.x &&
+        x <= obstacle.x + obstacle.width &&
+        y + grassHeight >= obstacle.y &&
+        y <= obstacle.y + obstacle.height,
+    )
+  );
+
+  return { x, y };
+}
+
+function drawGrass(obstacles: DOMRect[]) {
+  const { x, y } = getGrassPosition(obstacles);
   const alpha = centeredRandom();
 
   ctx.globalAlpha = alpha;
-  ctx.lineWidth = lineWidth;
-  ctx.strokeStyle = color;
+  ctx.lineWidth = grassLineWidth;
+  ctx.strokeStyle = grassColor;
 
   ctx.beginPath();
-  ctx.moveTo(x - width / 2, y - height / 1.25);
+  ctx.moveTo(x - grassWidth / 2, y - grassHeight / 1.25);
   ctx.lineTo(x, y);
-  ctx.lineTo(x + width / 2, y - height / 1.25);
+  ctx.lineTo(x + grassWidth / 2, y - grassHeight / 1.25);
   ctx.stroke();
 
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x, y - height);
+  ctx.lineTo(x, y - grassHeight);
   ctx.stroke();
 }
 
 function drawAllGrass() {
+  // 草の障害物
+  const avatarRect = avatar.getBoundingClientRect();
+  const obstacles = [avatarRect];
+
   for (let i = 0; i < total; i++) {
-    drawGrass();
+    drawGrass(obstacles);
   }
 }
 
